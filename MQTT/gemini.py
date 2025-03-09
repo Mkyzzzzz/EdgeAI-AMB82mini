4d4c5a0cb62c55be1f832536d453aeccc39369fc
Mkyzzzzz <126166534+Mkyzzzzz@users.noreply.github.com>
2025-03-09 23:13:53 +0800

    Update gemini.py

--- MQTT/gemini.py
+++ MQTT/gemini.py
@@ -26,5 +26,5 @@ response = model.generate_content( [prompt] )
 
 print(response.text)
 
-publish.single("ntou/01072114/edgeai/robot1", response.text, hostname=host)
+publish.single("ntou/edgeai/01072114/robot1", respons[e.text, hostname=host)
 
