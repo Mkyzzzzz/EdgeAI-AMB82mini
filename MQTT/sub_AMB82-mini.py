43ca63cedca0596f503cf016f626354e9cb28a60
Mkyzzzzz <126166534+Mkyzzzzz@users.noreply.github.com>
2025-03-09 23:08:36 +0800

    Update sub_AMB82-mini.py

--- MQTT/sub_AMB82-mini.py
+++ MQTT/sub_AMB82-mini.py
@@ -2,7 +2,7 @@ import paho.mqtt.subscribe as subscribe
 
 host = "test.mosquitto.org"
 
-msg = subscribe.simple("ntou/edgeai/robot1", hostname=host)
+msg = subscribe.simple("ntou/01072114/edgeai/robot1", hostname=host)
 print("topic  : %s" % (msg.topic))
 print()
 print("payload:")
