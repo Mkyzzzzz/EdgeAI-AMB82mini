c1ba3424380e1d9be3357fb37e46e125ea3111d2
Mkyzzzzz <126166534+Mkyzzzzz@users.noreply.github.com>
2025-03-09 23:19:24 +0800

    Update sub_AMB82-mini.py

--- MQTT/sub_AMB82-mini.py
+++ MQTT/sub_AMB82-mini.py
@@ -2,7 +2,7 @@ import paho.mqtt.subscribe as subscribe
 
 host = "test.mosquitto.org"
 
-msg = subscribe.simple("ntou/edgeai/01072114/[robot1", hostname=host)
+msg = subscribe.simple("ntou/edgeai/01072114/robot1", hostname=host)
 print("topic  : %s" % (msg.topic))
 print()
 print("payload:To turn around continuously, my motor will move like this:")
